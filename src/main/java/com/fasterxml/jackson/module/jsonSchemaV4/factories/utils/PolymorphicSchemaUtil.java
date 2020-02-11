a0c34a6e100e689cf6f94e0aff477d0b213931b7
Lee Butts <lee.butts@mahifx.com>
2020-02-11 16:02:17 +1300

    Set schema title to TypeName if available

--- src/main/java/com/fasterxml/jackson/module/jsonSchemaV4/factories/utils/PolymorphicSchemaUtil.java
+++ src/main/java/com/fasterxml/jackson/module/jsonSchemaV4/factories/utils/PolymorphicSchemaUtil.java
@@ -1,43 +1,26 @@
 package com.fasterxml.jackson.module.jsonSchemaV4.factories.utils;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationConfig;
-import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.introspect.AnnotatedClass;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 import com.fasterxml.jackson.databind.jsontype.NamedType;
-import com.fasterxml.jackson.module.jsonSchemaV4.SchemaGenerationContext;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 import com.fasterxml.jackson.module.jsonSchemaV4.JsonSchema;
+import com.fasterxml.jackson.module.jsonSchemaV4.SchemaGenerationContext;
 import com.fasterxml.jackson.module.jsonSchemaV4.factories.SchemaFactoryWrapper;
-import com.fasterxml.jackson.module.jsonSchemaV4.types.AnyOfSchema;
-import com.fasterxml.jackson.module.jsonSchemaV4.types.IntegerSchema;
-import com.fasterxml.jackson.module.jsonSchemaV4.types.NumberSchema;
-import com.fasterxml.jackson.module.jsonSchemaV4.types.PolymorphicObjectSchema;
-import com.fasterxml.jackson.module.jsonSchemaV4.types.ReferenceSchema;
-import com.fasterxml.jackson.module.jsonSchemaV4.types.StringSchema;
+import com.fasterxml.jackson.module.jsonSchemaV4.types.*;
 
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by zoliszel on 16/06/2015.
  */
 public class PolymorphicSchemaUtil {
 
-    public static class NamedJavaType{
+    public static class NamedJavaType {
         private final String name;
 
         private final JavaType javaType;
@@ -55,28 +38,28 @@ public class PolymorphicSchemaUtil {
             return name;
         }
 
-        public boolean hasName(){
-            return name!=null;
+        public boolean hasName() {
+            return name != null;
         }
 
-        public Class<?> getRawClass(){
+        public Class<?> getRawClass() {
             return javaType.getRawClass();
         }
     }
 
-    private static final Set<String>  ALLOWED_NON_NUMERIC_VALUES = new HashSet<String>(Arrays.asList("INF", "Infinity", "-INF", "-Infinity", "NaN"));
+    private static final Set<String> ALLOWED_NON_NUMERIC_VALUES = new HashSet<String>(Arrays.asList("INF", "Infinity", "-INF", "-Infinity", "NaN"));
 
     public static final String POLYMORPHIC_TYPE_NAME_SUFFIX = "_1";
 
-    public static final String NUMBER_WITH_NON_NUMERIC_NUMBER_REFERENCE ="___NUMBER___";
+    public static final String NUMBER_WITH_NON_NUMERIC_NUMBER_REFERENCE = "___NUMBER___";
 
-    public static final String NUMBER_WITH_NON_NUMERIC_ALLOWED_STRING_VALUES_REFERENCE ="__ALLOWED_NON_NUMERIC_VALUES___";
+    public static final String NUMBER_WITH_NON_NUMERIC_ALLOWED_STRING_VALUES_REFERENCE = "__ALLOWED_NON_NUMERIC_VALUES___";
 
-    public static final String NUMBER_WITH_NON_NUMERIC_VALUES="___NUMBER_WITH_NON_NUMERIC_VALUES___";
+    public static final String NUMBER_WITH_NON_NUMERIC_VALUES = "___NUMBER_WITH_NON_NUMERIC_VALUES___";
 
     private static final String DEFINITION_PREFIX = "#/definitions/";
 
-    private static JsonSchema getNumberSchemaWithAllowedString(){
+    private static JsonSchema getNumberSchemaWithAllowedString() {
         NumberSchema numberSchema = new NumberSchema();
         numberSchema.setId(SchemaGenerationContext.javaTypeToUrn(NUMBER_WITH_NON_NUMERIC_NUMBER_REFERENCE));
 
@@ -84,52 +67,49 @@ public class PolymorphicSchemaUtil {
         allowedStringValues.setId(SchemaGenerationContext.javaTypeToUrn(NUMBER_WITH_NON_NUMERIC_ALLOWED_STRING_VALUES_REFERENCE));
         allowedStringValues.setEnums(ALLOWED_NON_NUMERIC_VALUES);
 
-        Map<String,JsonSchema> typesToWrap = new HashMap<String, JsonSchema>();
-        typesToWrap.put(NUMBER_WITH_NON_NUMERIC_NUMBER_REFERENCE,numberSchema);
-        typesToWrap.put(NUMBER_WITH_NON_NUMERIC_ALLOWED_STRING_VALUES_REFERENCE,allowedStringValues);
+        Map<String, JsonSchema> typesToWrap = new HashMap<String, JsonSchema>();
+        typesToWrap.put(NUMBER_WITH_NON_NUMERIC_NUMBER_REFERENCE, numberSchema);
+        typesToWrap.put(NUMBER_WITH_NON_NUMERIC_ALLOWED_STRING_VALUES_REFERENCE, allowedStringValues);
         PolymorphicObjectSchema wrappedSchema = constructPolymorphicSchema(typesToWrap, PolymorphicObjectSchema.Type.ONE_OF);
         wrappedSchema.setId(SchemaGenerationContext.javaTypeToUrn(NUMBER_WITH_NON_NUMERIC_VALUES));
 
-        ReferenceSchema referenceSchema = new ReferenceSchema(DEFINITION_PREFIX +NUMBER_WITH_NON_NUMERIC_VALUES,wrappedSchema.getType());
+        ReferenceSchema referenceSchema = new ReferenceSchema(DEFINITION_PREFIX + NUMBER_WITH_NON_NUMERIC_VALUES, wrappedSchema.getType());
         referenceSchema.setDefinitions(new HashMap<String, JsonSchema>());
-        referenceSchema.getDefinitions().put(NUMBER_WITH_NON_NUMERIC_VALUES,wrappedSchema);
+        referenceSchema.getDefinitions().put(NUMBER_WITH_NON_NUMERIC_VALUES, wrappedSchema);
         return referenceSchema;
     }
 
-    public static PolymorphicObjectSchema constructPolymorphicSchema(Map<String, JsonSchema> typesToWrap,PolymorphicObjectSchema.Type type) {
+    public static PolymorphicObjectSchema constructPolymorphicSchema(Map<String, JsonSchema> typesToWrap, PolymorphicObjectSchema.Type type) {
 
         PolymorphicObjectSchema wrappedSchema = new PolymorphicObjectSchema();
         wrappedSchema.setDefinitions(new HashMap<String, JsonSchema>());
         List<ReferenceSchema> referenceSchemas = new ArrayList<ReferenceSchema>();
         Set<JsonFormatTypes> types = new HashSet<JsonFormatTypes>();
-        for(Map.Entry<String,? extends JsonSchema> entry : typesToWrap.entrySet()){
+        for (Map.Entry<String, ? extends JsonSchema> entry : typesToWrap.entrySet()) {
             final JsonSchema schema = entry.getValue();
-            if(schema == null) {
+            if (schema == null) {
                 throw new IllegalStateException("No schema for type [" + entry.getKey() + "]! All given types: " + typesToWrap);
             }
             wrappedSchema.getDefinitions().put(entry.getKey(), schema);
-            if(schema.isReferenceSchema()){
+            if (schema.isReferenceSchema()) {
                 referenceSchemas.add(schema.asReferenceSchema());
-            }
-            else {
+            } else {
                 referenceSchemas.add(new ReferenceSchema(DEFINITION_PREFIX + entry.getKey(), schema.getType()));
             }
-            if(schema.getType().isSingleJSONType()){
+            if (schema.getType().isSingleJSONType()) {
                 types.add(JsonFormatTypes.forValue(schema.getType().asSingleJsonType().getFormatType()));
-            }
-            else if(schema.getType().isArrayJSONType()){
+            } else if (schema.getType().isArrayJSONType()) {
                 types.addAll(Arrays.asList(schema.getType().asArrayJsonType().getFormatTypes()));
             }
         }
 
-        if(types.size()==1){
+        if (types.size() == 1) {
             wrappedSchema.setType(new JsonSchema.SingleJsonType(types.iterator().next()));
-        }
-        else{
+        } else {
             wrappedSchema.setType(new JsonSchema.ArrayJsonType(types.toArray(new JsonFormatTypes[types.size()])));
         }
-        ReferenceSchema[] referenceSchemaArray =referenceSchemas.toArray(new ReferenceSchema[referenceSchemas.size()]);
-        switch (type){
+        ReferenceSchema[] referenceSchemaArray = referenceSchemas.toArray(new ReferenceSchema[referenceSchemas.size()]);
+        switch (type) {
             case ANY_OF:
                 wrappedSchema.setAnyOf(referenceSchemaArray);
                 break;
@@ -140,8 +120,8 @@ public class PolymorphicSchemaUtil {
                 wrappedSchema.setOneOf(referenceSchemaArray);
                 break;
             case NOT:
-                default:
-                    throw new IllegalArgumentException("Unknonw polymorphic object type " + type);
+            default:
+                throw new IllegalArgumentException("Unknonw polymorphic object type " + type);
         }
         return wrappedSchema;
     }
@@ -165,22 +145,22 @@ public class PolymorphicSchemaUtil {
 
     public PolymorphicSchemaUtil(JavaType originalType, SerializerProvider provider) {
         this.originalType = originalType;
-        this.provider=provider;
-        if(provider!=null && provider.getConfig()!=null) {
+        this.provider = provider;
+        if (provider != null && provider.getConfig() != null) {
             Collection<NamedJavaType> namedTypes = extractSubTypes(originalType, getProvider().getConfig(), true);
 
             if (!namedTypes.isEmpty()) {
                 subTypes = namedTypes;
             }
         }
-        originalTypeName=getOriginalTypeName();
+        originalTypeName = getOriginalTypeName();
 
     }
 
     private String getOriginalTypeName() {
-        for(NamedJavaType namedJavaType :  subTypes){
-            if(namedJavaType.getRawClass() == originalType.getRawClass()){
-                if(namedJavaType.hasName()){
+        for (NamedJavaType namedJavaType : subTypes) {
+            if (namedJavaType.getRawClass() == originalType.getRawClass()) {
+                if (namedJavaType.hasName()) {
                     return namedJavaType.getName();
                 }
                 break;
@@ -189,7 +169,7 @@ public class PolymorphicSchemaUtil {
         return originalType.getRawClass().getSimpleName();
     }
 
-    public static Collection<NamedJavaType> extractSubTypes(JavaType type, SerializationConfig config,boolean removeNonConcrete) {
+    public static Collection<NamedJavaType> extractSubTypes(JavaType type, SerializationConfig config, boolean removeNonConcrete) {
         AnnotatedClass classWithoutSuperType = AnnotatedClass.constructWithoutSuperTypes(type.getRawClass(), config.getAnnotationIntrospector(), config);
         Collection<NamedType> subTypes = null;
         if (config.getSubtypeResolver() != null) {
@@ -198,26 +178,26 @@ public class PolymorphicSchemaUtil {
         }
 
         List<NamedJavaType> result = new ArrayList<NamedJavaType>();
-        for(NamedType subType : subTypes){
+        for (NamedType subType : subTypes) {
             //remove abstract classes/intefaces when requested, should have full type information in subclasses
-            boolean addType=true;
-            if(removeNonConcrete && Modifier.isAbstract(subType.getType().getModifiers()) && !subType.getType().isArray()){
-                addType=false;
+            boolean addType = true;
+            if (removeNonConcrete && Modifier.isAbstract(subType.getType().getModifiers()) && !subType.getType().isArray()) {
+                addType = false;
             }
             if (removeNonConcrete && Modifier.isInterface(subType.getType().getModifiers())) {
-                addType=false;
+                addType = false;
             }
-            if(addType){
-                result.add(new NamedJavaType(subType.hasName() ? subType.getName() : null,type.narrowBy(subType.getType())));
+            if (addType) {
+                result.add(new NamedJavaType(subType.hasName() ? subType.getName() : null, type.narrowBy(subType.getType())));
             }
         }
 
         return result;
     }
+
     public SerializerProvider getProvider() {
         return provider;
     }
-
 
 
     public PolymorphiSchemaDefinition extractPolyMorphicObjectSchema() throws JsonMappingException {
@@ -233,11 +213,17 @@ public class PolymorphicSchemaUtil {
             if (!namedType.hasName()) {
                 throw new IllegalArgumentException("No name associated with class " + namedType.getRawClass().getSimpleName() + " try using @JsonTypeName annotation");
             }
-            JsonSchema subSchema = schema(namedType.getJavaType());
-            String subTypeName =namedType.getName();
+            JavaType subJavaType = namedType.getJavaType();
+            JsonSchema subSchema = schema(subJavaType);
+            String subTypeName = namedType.getName();
 
-            if(namedType.getRawClass() == originalType.getRawClass()){
+            if (namedType.getRawClass() == originalType.getRawClass()) {
                 subTypeName = subTypeName + POLYMORPHIC_TYPE_NAME_SUFFIX;
+            }
+
+            TypeSerializer typeSerializer = getProvider().findTypeSerializer(subJavaType);
+            if (typeSerializer != null) {
+                subTypeName = typeSerializer.getTypeIdResolver().idFromBaseType();
             }
 
             subSchemas[i] = subSchema;
@@ -245,8 +231,8 @@ public class PolymorphicSchemaUtil {
             definitions.put(subTypeName, subSchemas[i]);
         }
 
-       final PolymorphicObjectSchema wrapperSchema =constructPolymorphicSchema(definitions,PolymorphicObjectSchema.Type.ANY_OF);
-        return new PolymorphiSchemaDefinition(){
+        final PolymorphicObjectSchema wrapperSchema = constructPolymorphicSchema(definitions, PolymorphicObjectSchema.Type.ANY_OF);
+        return new PolymorphiSchemaDefinition() {
 
             @Override
             public JsonSchema getPolymorphicObjectSchema() {
@@ -269,14 +255,15 @@ public class PolymorphicSchemaUtil {
         return subTypes.size() > 1;
     }
 
-    public static JsonSchema propagateDefinitionsUp(JsonSchema node){
-        Map<String,JsonSchema> allDefinitions = extractDefinitions(node,new HashMap<String, JsonSchema>());
-        if(!allDefinitions.isEmpty()){
+    public static JsonSchema propagateDefinitionsUp(JsonSchema node) {
+        Map<String, JsonSchema> allDefinitions = extractDefinitions(node, new HashMap<String, JsonSchema>());
+        if (!allDefinitions.isEmpty()) {
             node.setDefinitions(allDefinitions);
         }
         return node;
 
     }
+
     protected JsonSchema schema(Type t) throws JsonMappingException {
         SchemaGenerationContext context = SchemaGenerationContext.get();
         ObjectMapper mapper = context.getMapper().copy();
@@ -286,68 +273,67 @@ public class PolymorphicSchemaUtil {
     }
 
     public static JsonSchema wrapNonNumericTypes(JsonSchema originalSchema) {
-        if(!originalSchema.isNumberSchema()){
+        if (!originalSchema.isNumberSchema()) {
             return originalSchema;
         }
 
-        if(originalSchema instanceof IntegerSchema){
+        if (originalSchema instanceof IntegerSchema) {
             return originalSchema;
         }
-        if(!SchemaGenerationContext.get().getMapper().isEnabled(JsonParser.Feature.ALLOW_NON_NUMERIC_NUMBERS)){
+        if (!SchemaGenerationContext.get().getMapper().isEnabled(JsonParser.Feature.ALLOW_NON_NUMERIC_NUMBERS)) {
             return originalSchema;
         }
 
-        NumberSchema numberSchema = (NumberSchema)originalSchema;
-        if(numberSchema.getEnums()!=null && !numberSchema.getEnums().isEmpty()){
+        NumberSchema numberSchema = (NumberSchema) originalSchema;
+        if (numberSchema.getEnums() != null && !numberSchema.getEnums().isEmpty()) {
             return numberSchema;
         }
 
         return getNumberSchemaWithAllowedString();
 
     }
-    private static Map<String,JsonSchema> extractDefinitions(JsonSchema node,Map<String,JsonSchema> definitionsSoFar){
-        if(node.isObjectSchema()){
-            if(node.asObjectSchema().getProperties()!=null) {
+
+    private static Map<String, JsonSchema> extractDefinitions(JsonSchema node, Map<String, JsonSchema> definitionsSoFar) {
+        if (node.isObjectSchema()) {
+            if (node.asObjectSchema().getProperties() != null) {
                 for (Map.Entry<String, JsonSchema> entry : node.asObjectSchema().getProperties().entrySet()) {
-                    mergeDefinitions(definitionsSoFar,extractDefinitions(entry.getValue(), definitionsSoFar));
+                    mergeDefinitions(definitionsSoFar, extractDefinitions(entry.getValue(), definitionsSoFar));
                 }
             }
-            if(node.asObjectSchema().getAdditionalProperties()!=null) {
+            if (node.asObjectSchema().getAdditionalProperties() != null) {
                 if (node.asObjectSchema().getAdditionalProperties().isSchemaAdditionalProperties()) {
                     JsonSchema additionalProps = node.asObjectSchema().getAdditionalProperties().asSchemaAdditionalProperties().getJsonSchema();
                     mergeDefinitions(definitionsSoFar, extractDefinitions(additionalProps, definitionsSoFar));
                 }
             }
-        }
-        else if(node.isArraySchema()){
-            if(node.asArraySchema().getItems()!=null){
-                if(node.asArraySchema().getItems().isSingleItems()){
-                    mergeDefinitions(definitionsSoFar,extractDefinitions(node.asArraySchema().getItems().asSingleItems().getSchema(),definitionsSoFar));
-                }
-                else if(node.asArraySchema().getItems().isArrayItems()){
-                    for(JsonSchema schema : node.asArraySchema().getItems().asArrayItems().getJsonSchemas()){
-                        mergeDefinitions(definitionsSoFar,extractDefinitions(schema,definitionsSoFar));
+        } else if (node.isArraySchema()) {
+            if (node.asArraySchema().getItems() != null) {
+                if (node.asArraySchema().getItems().isSingleItems()) {
+                    mergeDefinitions(definitionsSoFar, extractDefinitions(node.asArraySchema().getItems().asSingleItems().getSchema(), definitionsSoFar));
+                } else if (node.asArraySchema().getItems().isArrayItems()) {
+                    for (JsonSchema schema : node.asArraySchema().getItems().asArrayItems().getJsonSchemas()) {
+                        mergeDefinitions(definitionsSoFar, extractDefinitions(schema, definitionsSoFar));
                     }
                 }
             }
         }
-        if(node.getDefinitions()!=null) {
-            for(Map.Entry<String,JsonSchema> entry : node.getDefinitions().entrySet()){
-                mergeDefinitions(definitionsSoFar,extractDefinitions(entry.getValue(),definitionsSoFar));
+        if (node.getDefinitions() != null) {
+            for (Map.Entry<String, JsonSchema> entry : node.getDefinitions().entrySet()) {
+                mergeDefinitions(definitionsSoFar, extractDefinitions(entry.getValue(), definitionsSoFar));
             }
-            mergeDefinitions(definitionsSoFar,node.getDefinitions());
+            mergeDefinitions(definitionsSoFar, node.getDefinitions());
             node.setDefinitions(null);
         }
         return definitionsSoFar;
     }
 
-    private static void mergeDefinitions(Map<String,JsonSchema> target, Map<String,JsonSchema> source){
-        if(source==null){
+    private static void mergeDefinitions(Map<String, JsonSchema> target, Map<String, JsonSchema> source) {
+        if (source == null) {
             return;
         }
-        for(Map.Entry<String,JsonSchema> entry : source.entrySet()){
-            if(!(entry.getValue() instanceof ReferenceSchema) && !(entry.getValue() instanceof AnyOfSchema)){
-                target.put(entry.getKey(),entry.getValue());
+        for (Map.Entry<String, JsonSchema> entry : source.entrySet()) {
+            if (!(entry.getValue() instanceof ReferenceSchema) && !(entry.getValue() instanceof AnyOfSchema)) {
+                target.put(entry.getKey(), entry.getValue());
             }
         }
     }
